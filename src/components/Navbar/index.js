87768313a65d1815b25f812254d237096c370c47
Mansi V Jain <mansi.cs20@bmsce.ac.in>
2023-02-09 12:26:20 +0530

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,142 @@
+import React from 'react' ;
+import icon from "./icon.png";
+import {Nav, NavLink, Bars, NavMenu} from './Navbarelements';
+
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavMenu>
+          <NavLink to="" activeStyle>
+            File
+          </NavLink>
+          <NavLink to="" activeStyle>
+            Edit
+          </NavLink>
+          <NavLink to="" activeStyle>
+            Run
+          </NavLink>
+          <NavLink to="" activeStyle>
+            Configuration
+          </NavLink>
+          <NavLink to="" activeStyle>
+            Result
+          </NavLink>
+          <NavLink to="" activeStyle>
+            Windows
+          </NavLink>
+          <NavLink to="" activeStyle>
+            Help
+          </NavLink>
+        </NavMenu>
+      </Nav>
+      <Nav>
+        
+        
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>   
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+        <div className="second">
+            <div className='button'>
+                <img className="icon" src={icon} />
+            </div>     
+        </div>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
